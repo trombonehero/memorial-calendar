dfa0bead008ba086bfd23b239d1336840b7d35dc
Jonathan Anderson <jonathan.anderson@ieee.org>
2017-01-11 14:29:28 -0330

    Handle multiple input files.

--- graph-courses.py
+++ graph-courses.py
@@ -7,15 +7,13 @@ import dot
 import sys
 
 args = argparse.ArgumentParser()
-args.add_argument('filename')
+args.add_argument('filename', nargs = '+')
 args.add_argument('--format', choices = [ 'calendar', 'dot' ], default = 'dot')
 args.add_argument('-o', '--output', default = '-')
 
 args = args.parse_args()
 output = sys.stdout if args.output == '-' else open(args.output, 'w')
 
-
-courses = calendar.parse(open(args.filename), prefix = 'ENGI')
 
 colours = collections.defaultdict(lambda: '#00000000')
 colours[0] = '#00ffff5f'   # ONAE
@@ -27,17 +25,21 @@ colours[7] = '#0000005f'   # Civil
 colours[8] = '#0000ff5f'   # ECE
 colours[9] = '#ff00005f'   # Mechanical
 
-for course in courses.values():
-	if course['name'].startswith('ENGI '):
-		num = str(course['number'])
-		term = int(num[0])
-		department = int(num[1])
+courses = {}
+for filename in args.filename:
+	courses.update(calendar.parse(open(filename), prefix = 'ENGI'))
 
-		if term == 1:
-			course['colour'] = '#00000020'
+	for course in courses.values():
+		if course['name'].startswith('ENGI '):
+			num = str(course['number'])
+			term = int(num[0])
+			department = int(num[1])
 
-		else:
-			course['colour'] = colours[department]
+			if term == 1:
+				course['colour'] = '#00000020'
+
+			else:
+				course['colour'] = colours[department]
 
 
 if args.format == 'calendar':
