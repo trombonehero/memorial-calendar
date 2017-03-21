5a52fa391cf29c2dded2f64a330259f916ae3606
Jonathan Anderson <jonathan.anderson@ieee.org>
2017-03-21 14:36:46 -0230

    Add license comment to each Python file.

--- graph-courses.py
+++ graph-courses.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python
 
+# Copyright 2014, 2017 Jonathan Anderson
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
 import argparse
 import calendar as cal
 import collections
@@ -15,16 +29,6 @@ args = args.parse_args()
 output = sys.stdout if args.output == '-' else open(args.output, 'w')
 
 
-colours = collections.defaultdict(lambda: '#00000000')
-colours[0] = '#00ffff5f'   # ONAE
-colours[1] = '#00ff005f'   # The engineering profession
-colours[3] = '#ff99005f'   # Mechanics and thermo
-colours[4] = '#ff00ff5f'   # Math
-colours[6] = '#ffff005f'   # Process
-colours[7] = '#0000005f'   # Civil
-colours[8] = '#0000ff5f'   # ECE
-colours[9] = '#ff00005f'   # Mechanical
-
 courses = collections.defaultdict(dict)
 courses.update(cal.pseudo_prerequisites())
 
@@ -36,24 +40,4 @@ for filename in args.filename:
 	for (name,c) in parse(open(filename), prefix = 'ENGI').items():
 		courses[name].update(c)
 
-	for course in courses.values():
-		if course['name'].startswith('ENGI '):
-			num = str(course['number'])
-			term = int(num[0])
-			department = int(num[1])
-
-			if term == 1:
-				course['colour'] = '#00000020'
-
-			else:
-				course['colour'] = colours[department]
-
-
-if args.format == 'calendar':
-	calendar.format(courses, output)
-
-elif args.format == 'dot':
-	dot.format(courses, output)
-
-else:
-	raise ValueError("bad output format '%s'" % args.format)
+print(courses)
